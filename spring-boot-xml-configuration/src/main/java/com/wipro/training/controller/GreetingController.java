53fda87e4d1bf73c8b5d8d9316aa92f25a59a1d7
Amorim, Xenia <e047418@mastercard.com>
2019-01-22 08:53:30 -0200

    Remove useless @Autowired from controller class

--- spring-boot-xml-configuration/src/main/java/com/wipro/training/controller/GreetingController.java
+++ spring-boot-xml-configuration/src/main/java/com/wipro/training/controller/GreetingController.java
@@ -1,13 +1,11 @@
 package com.wipro.training.controller;
 
 import com.wipro.training.service.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class GreetingController {
 
     private GreetingService greetingService;
 
-    @Autowired
     public GreetingController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
